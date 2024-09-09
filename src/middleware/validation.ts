1c60c1374e2946991e1bbd82e9f175c0e0b7e99e
Claudiu-c <claudiu.capatana1@gmail.com>
2024-09-09 19:08:19 +0300

    adding user profile feature

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -23,3 +23,27 @@ export const validateMyUserRequest = [
   body("country").isString().notEmpty().withMessage("Country must be a string"),
   handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("city").notEmpty().withMessage("City name is required"),
+  body("country").notEmpty().withMessage("Country name is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a positive integer"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array can not be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be a positive number"),
+  handleValidationErrors,
+];
---
+++ src/models/restaurant.ts
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const menuItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+const restaurantSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  restaurantName: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  deliveryPrice: { type: String, required: true },
+  estimatedDeliveryTime: { type: String, required: true },
+  cuisines: [{ type: String, required: true }],
+  menuItems: [menuItemSchema],
+  imageUrl: { type: String, required: true },
+  lastUpdated: { type: Date, required: true },
+});
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+export default Restaurant;
